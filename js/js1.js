49fd130e27f217c93fad2ef51fc05091030c96e1
tori1021 <31543929+tori1021@users.noreply.github.com>
2017-11-15 01:32:45 +0300

    24

--- js/js1.js
+++ js/js1.js
@@ -35,24 +35,82 @@ close.addEventListener('click', function (evt){
 });
 //*конец Напишите нам*
 //*переключатели слайдера*
-var label1=document.querySelector('#label1');
-var label2=document.querySelector('#label2');
-var label3=document.querySelector('#label3');
-var Decoration=document.querySelectorAll('.Decoration');
+var label1=document.querySelector('#lbl1');
+var label2=document.querySelector('#lbl2');
+var label3=document.querySelector('#lbl3');
+var Decor=document.querySelectorAll('.decoration');
 
 label1.addEventListener('click',function(){
-    Decoration[0].style.background='white';
-    Decoration[1].style.background='none';
-    Decoration[2].style.background='none';
+    Decor[0].style.background='white';
+    Decor[1].style.background='none';
+    Decor[2].style.background='none';
 });
 label2.addEventListener('click',function(){
-    Decoration[0].style.background='none';
-    Decoration[1].style.background='white';
-    Decoration[2].style.background='none';
+    Decor[0].style.background='none';
+    Decor[1].style.background='white';
+    Decor[2].style.background='none';
 });
 label3.addEventListener('click',function(){
-    Decoration[0].style.background='none';
-    Decoration[1].style.background='none';
-    Decoration[2].style.background='white';
+    Decor[0].style.background='none';
+    Decor[1].style.background='none';
+    Decor[2].style.background='white';
 });
 //*конец переключатели слайдера*
+
+//*каталог*
+var element1=document.querySelector('.element1');
+var element2=document.querySelector('.element2');
+var element3=document.querySelector('.element3');
+
+element1.addEventListener('mouseover', function (evt) {
+    evt.preventDefault();
+    element1.getElementsByTagName('img')[0].classList.add('hover-products');
+});
+element1.addEventListener('mouseleave', function (evt) {
+    evt.preventDefault();
+    element1.getElementsByTagName('img')[0].classList.remove('hover-products');
+});
+element1.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+    element1.getElementsByTagName('img')[0].classList.add('hover-products');
+    element1.style.opacity='0.5';
+});
+element1.addEventListener('mouseup', function (evt) {
+    evt.preventDefault();
+    element1.style.opacity='1';
+});
+element2.addEventListener('mouseover', function (evt) {
+    evt.preventDefault();
+    element2.getElementsByTagName('img')[0].classList.add('hover-products');
+});
+element2.addEventListener('mouseleave', function (evt) {
+    evt.preventDefault();
+    element2.getElementsByTagName('img')[0].classList.remove('hover-products');
+});
+element2.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+    element2.getElementsByTagName('img')[0].classList.add('hover-products');
+    element2.style.opacity='0.5';
+});
+element2.addEventListener('mouseup', function (evt) {
+    evt.preventDefault();
+    element2.style.opacity='1';
+});
+element3.addEventListener('mouseover', function (evt) {
+    evt.preventDefault();
+    element3.getElementsByTagName('img')[0].classList.add('hover-products');
+});
+element3.addEventListener('mouseleave', function (evt) {
+    evt.preventDefault();
+    element3.getElementsByTagName('img')[0].classList.remove('hover-products');
+});
+element3.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+    element3.getElementsByTagName('img')[0].classList.add('hover-products');
+    element3.style.opacity='0.5';
+});
+element3.addEventListener('mouseup', function (evt) {
+    evt.preventDefault();
+    element3.style.opacity='1';
+});
+//*конец каталог* 
